ea6cb86f5e8a2f52e4624723aba1e50bc1be92ff
Ashish Singh <aitashish173@gmail.com>
2015-04-11 21:38:12 +0530

    Added Location map setting
    * Added location map in settings(show: never)
    * This opens application with geo tag and data as post code.

--- app/src/main/java/com/example/comrade/sunshine/MainActivity.java
+++ app/src/main/java/com/example/comrade/sunshine/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.comrade.sunshine;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -33,6 +36,8 @@ import static com.example.comrade.sunshine.R.*;
 
 public class MainActivity extends ActionBarActivity {
 
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,12 +66,36 @@ public class MainActivity extends ActionBarActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             startActivity(new Intent(this, SettingsActivity.class));
-
+            return true;
+        }
+        if(id == R.id.action_map) {
+            openPreferredLocationInMap();
             return true;
         }
         // This handles the cases when none of if conditions are true.
         return super.onOptionsItemSelected(item);
     }
 
+    private void openPreferredLocationInMap(){
+        // fetch the location data from sharedPreferences
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String location = sharedPrefs.getString(
+                getString(R.string.pref_location_key),
+                getString(string.pref_location_default)
+        );
+        // once we have location we can call the intent.
+        Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                .appendQueryParameter("q", location)
+                .build();
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+        // It's important we check for apps on system which can handle
+        // such requests to prevent app crash
+        if(intent.resolveActivity(getPackageManager()) != null ){
+            startActivity(intent);
+        }else{
+            Log.d(LOG_TAG, "Couldn't call " + location + ", no");
+        }
+    }
 
 }
